aa32034b7c078540baba624d6ae0072fbfbc5a2f
Saugat Thapaliya <thapaliyasaugat99@gmail.com>
2022-09-24 22:41:08 +0545

    branch cashback created

--- src/main/java/com/securitytest/securitytest/serviceImpl/CashbackServiceImpl.java
+++ src/main/java/com/securitytest/securitytest/serviceImpl/CashbackServiceImpl.java
@@ -2,10 +2,14 @@ package com.securitytest.securitytest.serviceImpl;
 
 import com.securitytest.securitytest.resource.*;
 import com.securitytest.securitytest.service.CashbackService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 public class CashbackServiceImpl implements CashbackService {
     @Override
     public ApiResponse<CashbackSchemeDto> createCashBackScheme(CashbackRequest cashbackRequest) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
         return null;
     }
 
